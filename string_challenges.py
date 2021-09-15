68be768d609f2dd5177a2b6351200530dfbf8a60
Aleksei Petrov <aleksei.petrov@five9.com>
2021-09-15 12:01:51 +0300

    Revert "All exercises are done"
    This reverts commit 7805dccfc9ef0e8f471814b3625a19cf9ac5d253.

--- string_challenges.py
+++ string_challenges.py
@@ -1,46 +1,28 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-print(word[-1])
+# ???
 
 
 # Вывести количество букв "а" в слове
-counter = 0
 word = 'Архангельск'
-for letter in word:
-    if letter.lower() == 'а':
-        counter += 1
-print(f'Word {word} has {counter} \'a\' char(s) \n')
+# ???
 
 
 # Вывести количество гласных букв в слове
-
 word = 'Архангельск'
-vowels =['а', 'о', 'э', 'е', 'и', 'ы', 'у', 'ё', 'ю', 'я']
-vowels_counter = 0
-for letter in word:
-    if letter.lower() in vowels:
-        vowels_counter += 1
-print(f'Word {word} has {vowels_counter} vowel(s) \n')
+# ???
+
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-words = sentence.split()
-print(f'Sentence \"{sentence}\" has  {len(words)} word(s) \n')
+# ???
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-words = sentence.split()
-for word in words:
-    print(f'The first letter of the word \"{word}\" is {word[0]} \n')
+# ???
+
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-words = sentence.split()
-total = 0
-counter = 0
-for word in words:
-    total += len(word)
-    counter += 1
-avg = total / counter
-print(f'The average word lenght is {avg}')
+# ???
